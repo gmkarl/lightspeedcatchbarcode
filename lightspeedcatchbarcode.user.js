1aab8053a6ab14ee96cf9faaaefe6b42029d9792
Karl Semich <gmkarl@gmail.com>
2015-11-23 10:54:46 -0500

    Don't rewrite UPC-E as UPC-A; it's not done when you enter it normally

--- lightspeedcatchbarcode.user.js
+++ lightspeedcatchbarcode.user.js
@@ -58,7 +58,7 @@ function isUPCE(d) {
         n = [d[0],d[1],d[2],d[3],d[4],d[5],0,0,0,   0,   d[6],d[7]];
 
     if (isEAN(n)) {
-        lastKeys = n;
+        //lastKeys = n;
         return true;
     } else {
         return false;
